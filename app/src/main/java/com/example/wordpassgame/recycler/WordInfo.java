06714526ba03671f7df416a790f48bbe6327327e
Arakene <kbh97102@naver.com>
2020-06-24 20:52:16 +0900

    Update WordInfo

--- app/src/main/java/com/example/wordpassgame/recycler/WordInfo.java
+++ app/src/main/java/com/example/wordpassgame/recycler/WordInfo.java
@@ -3,4 +3,9 @@ package com.example.wordpassgame.recycler;
 public class WordInfo {
     public String word;
     public int type;
+
+    public WordInfo(String word, int type) {
+        this.word = word;
+        this.type = type;
+    }
 }
