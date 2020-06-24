a261aa879d5ee2f0f6c0b514b51066f46b69f2b4
Arakene <kbh97102@naver.com>
2020-06-24 20:00:28 +0900

    Add RecyclerView

---
+++ app/src/main/java/com/example/wordpassgame/recycler/RecyclerHolder.java
@@ -0,0 +1,13 @@
+package com.example.wordpassgame.recycler;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
